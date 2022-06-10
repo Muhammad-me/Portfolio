60caef501e97fffafb0587525589f97b13db62bb
Muhammad-me <mmajidhanov@gmail.com>
2022-06-10 18:51:38 +0500

    I upload my js

--- index.js
+++ index.js
@@ -1,2 +1,44 @@
-var ism = prompt("Hello Welcome To My Site. Please Write Your Name!")
-alert("Welcome To My Site!")
+let ism = prompt("Hello. Please enter your name."); 
+let ismlar = ["Jakhongir", "Murodjon", "Boburjon", "Azimjon" ,"Akmalxon", "Farruhbek", "Kamoldin", "Muhammad", "MuhammadYusuf", "Vorisxon", "Hikmatillo"];
+if (ism == ismlar[0]){
+    alert(`Welcome ${ismlar[0]},Please enter your age.`);
+}else if (ism == ismlar[1]){
+    alert(`Welcome ${ismlar[1]},Please enter your age.`);  
+}else if (ism == ismlar[2]) {   
+     alert(`Welcome ${ismlar[2]},Please enter your age.`);
+}else if (ism == ismlar[3]) {   
+    alert(`Welcome ${ismlar[3]},Please enter your age.`);
+}else if (ism == ismlar[4]) {   
+    alert(`Welcome ${ismlar[4]},Please enter your age.`);
+}else if (ism == ismlar[5]) {   
+    alert(`Welcome ${ismlar[5]},Please enter your age.`);
+}else if (ism == ismlar[6]) {   
+    alert(`Welcome ${ismlar[6]},Please enter your age.`);
+}else if (ism == ismlar[7]) {   
+    alert(`Welcome ${ismlar[7]},Please enter your age.`);
+}else if (ism == ismlar[8]) {   
+    alert(`Welcome ${ismlar[8]},Please enter your age.`);
+}else if (ism == ismlar[9]) {   
+    alert(`Welcome ${ismlar[9]},Please enter your age.`);
+}else if (ism == ismlar[10]){   
+    alert(`Welcome ${ismlar[10]},Please enter your age.`);
+}
+if (ism == ""){
+    alert("You must enter your name");
+}
+let yosh = prompt("Please Enter Your Year.");
+if (yosh < 2004){
+    alert("Please enter your e-mail");
+}
+if (yosh > 2004){
+    alert("You are not old enough.");
+}
+if (yosh == ""){
+    alert("You must enter your year");
+}
+let email = prompt("Please enter your e-mail");
+if (email == ""){
+    alert("  You must enter your e-mail");
+}
+alert(`Welcome to my Portfolio`);
+
